591159e4a02c38415cd1324013e935c9dd50eaea
jellujoa <jellujoa03@gmail.com>
2025-06-12 19:28:59 +0900

    add spawnenemy, stagedata,stagemanager fix enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,18 +1,20 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public EnemyData enemyData;  // 에디터에서 할당
+
+    private int currentHealth;
+
     void Start()
     {
-        
-    }
+        // 현재 스테이지 번호 가져오기 (StageManager에서)
+        StageManager stageManager = FindObjectOfType<StageManager>();
+        int currentStageNumber = stageManager != null ? stageManager.currentStage.stageNumber : 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // 스테이지에 맞게 체력 세팅
+        currentHealth = enemyData.GetHealthForStage(currentStageNumber);
     }
 }
--- Assets/Scripts/Enemy/StageManager.cs
+++ Assets/Scripts/Enemy/StageManager.cs
@@ -1,50 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    public float stageTime = 90f; // 스테이지 시간 
-    public float timeReamaing; // 남은 시간
-    public int currentStage = 1; // 현재 스테이지
-    public int bossStage = 50; // 보스 스테이지
+    [Header("StageData (ScriptableObject)")]
+    public List<StageData> stages;
 
-    public int enemykilled = 0; // 처치한 적 수에 따라 스테이지를 올리는 방식을 생각중 !
-    public int killedForNextStage = 5; // 다음 스테이지로 넘어가기 위한 적 처치 수
+    private int currentStageIndex = 0; 
+    public StageData currentStage;
 
-  
+    private float timeRemaining;
+    private int enemyKill = 0;
 
-    public void Awake()
-    {
-        
-    }
     void Start()
     {
-
+        LoadStage(currentStageIndex);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (timeReamaing > 0)
+        if (currentStage == null) return;
+
+        // 시간 감소
+        if (timeRemaining > 0)
         {
-            timeReamaing -= Time.deltaTime; // 남은 시간 감소
+            timeRemaining -= Time.deltaTime;
         }
         else
         {
-            ResetStage(); // 시간이 다 되면 다음 스테이지로 넘어감
+            // 시간이 다 되었을 때 조건 분기
+            if (enemyKill >= currentStage.killsToNext)
+            {
+                // 다 죽였다면 다음 스테이지
+                NextStage();
+            }
+            else
+            {
+                // 못 죽였다면 현재 스테이지 반복
+                RetryStage();
+            }
         }
     }
-    public void ResetStage()
-    {
-        if (timeReamaing <= 0)
-        {
-            timeReamaing = 90;
-            enemykilled = 0; // 적 처치 수 초기화
-           //enemycs에 currenthp 를 저장하고 그 걸 초기화 해야함 ! 
-           //data를 건들면 모든게 망가질 수 있다 
 
+    public void OnEnemyKilled()
+    {
+        enemyKill++;
+
+        if (enemyKill >= currentStage.killsToNext)
+        {
+            // 적을 다 죽이면 즉시 다음 스테이지로 넘어감
+            NextStage();
         }
+    }
+
+    void LoadStage(int index)
+    {
+        currentStage = stages[index];
+        timeRemaining = currentStage.stageDuration;
+        enemyKill = 0;  
+    }
+
+    void NextStage()
+    {
+        currentStageIndex++;
+        LoadStage(currentStageIndex);
+    }
+
+    void RetryStage()
+    {
+        
+        LoadStage(currentStageIndex); // 현재 스테이지 다시 시작
     }
 }
