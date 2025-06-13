0eb0343082470fd4caad4c5ae2b0f4e704802496
피타쵸쵸 <djfb123@naver.com>
2025-06-13 16:45:08 +0900

    [feat] Critical % added
    [feat] Critical % added

--- Assets/Scripts/AttackManager/Attack.cs
+++ Assets/Scripts/AttackManager/Attack.cs
@@ -12,7 +12,7 @@ public class Attack : MonoBehaviour
     public WeaponData weaponData;
 
     public int AutoAttacklevel = 0;// 자동공격
-
+    public int CriticalLevel = 1;
     public float TouchAttackCoolDown = 0.2f;//터치 쿨다운
     public float LastAtkTouch;
     public bool IsAttackTouch = true;
@@ -58,21 +58,31 @@ public class Attack : MonoBehaviour
     }
     IEnumerator AutoAttackRoutine()
     {
-        
+        while (true)
+        {
+            // 공격시 데미지 계산 추가
             float delay = GetAutoAttackDelay();
             yield return new WaitForSeconds(delay);
-        
+        }
     }
     float GetAutoAttackDelay()
     {
         return Mathf.Max(0.2f, 2.0f - 0.1f * AutoAttacklevel);
     }
 
-    //private bool IsOccur(int prob)
-    //{
-    //    int isOccur = new Random().Next(0, 100);
-    //    if (isOccur < prob) return true;
-    //    else return false;
-    //} 치명타
+
+    void AttackMonster()
+    {
+        int damage = IsCriticalAtk(int.Parse("0.5")) ? playerData.attack * CriticalLevel  : playerData.attack;
+        enemyData.enemyHealth -= (damage + weaponData.bonusDamage);
+    }
+
+    bool IsCriticalAtk(float criticPer)
+    {
+        float number = Random.value;
+        //Random.Range(0, 100);
+        return (number >= criticPer); //true false 에 따라처리
+    }
+    // 치명타 확률, 치명타배율
 }
 
