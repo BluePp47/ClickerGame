9401a3557336cac10ae9a5d0ac5d39ae649b8205
myosotis0423 <shellingford52@gmail.com>
2025-06-18 16:06:26 +0900

    fix currencymanager

--- Assets/Scripts/AllGameManager/CurrencyManager.cs
+++ Assets/Scripts/AllGameManager/CurrencyManager.cs
@@ -4,25 +4,20 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CurrencyManager 
+public class CurrencyManager : MonoBehaviour
 {
-    private PlayerData playerData;
-    private TextMeshProUGUI goldText; //UGUI 텍스트로 골드표시
-    private MonoBehaviour coroutineHost;
-
-    public CurrencyManager(PlayerData data, TextMeshProUGUI goldText, MonoBehaviour host)
+    public PlayerData playerData;
+    public TextMeshProUGUI goldText; //UGUI 텍스트로 골드표시
+    public float waitTime = 0.02f; // 텍스트가 변경되는 속도<= float waitTime = 0.02f; //숫자를 변경할 때 걸리는 시간
+    void Start()
     {
-        this.playerData = data;
-        this.goldText = goldText;
-        this.coroutineHost = host;
-
         UpdateGoldUI();
     }
 
     public void AddGold(int amount)
     {
         playerData.gold += amount;
-        coroutineHost.StartCoroutine(GoldChange());
+        StartCoroutine(GoldChange());
     }
 
     public bool SubtractGold(int amount)
@@ -30,7 +25,7 @@ public class CurrencyManager
         if (playerData.gold >= amount)
         {
             playerData.gold -= amount;
-            coroutineHost.StartCoroutine(GoldChange());
+            StartCoroutine(GoldChange());
             return true;
         }
         return false;
@@ -42,11 +37,11 @@ public class CurrencyManager
 
     private IEnumerator GoldChange()
     {
-        int startValue = int.Parse(goldText.text); //시작 시 현재 텍스트에 표시된 골드 값을 반영
+        int startValue = int.Parse(goldText.text.Replace("Gold: ", "")); //시작 시 현재 텍스트에 표시된 골드 값을 반영
 
         int endValue = playerData.gold; //끝났을때 실제 보유한 재화
 
-        float waitTime = 0.02f; //숫자를 변경할 때 걸리는 시간
+        
 
         //골드가 증가인지 감소인지 확인
         if (startValue < endValue) //골드증가
@@ -54,7 +49,7 @@ public class CurrencyManager
             
             for (int i = startValue; i <= endValue; i++)//for문으로 숫자를 1 씩 변화, 눈에 보이는 효과
             {
-                goldText.text = i.ToString(); // 텍스트 UI 업데이트
+                goldText.text = $"Gold: {i}"; // 텍스트 UI 업데이트
                 yield return new WaitForSeconds(waitTime); // 대기
             }
         }
@@ -62,7 +57,7 @@ public class CurrencyManager
         {
             for (int i = startValue; i >= endValue; i--)// 골드가 감소할 때 
             {
-                goldText.text = i.ToString(); // 텍스트 UI 업데이트
+                goldText.text = $"Gold: {i}"; // 텍스트 UI 업데이트
                 yield return new WaitForSeconds(waitTime); // 대기
             }
         }
@@ -70,10 +65,7 @@ public class CurrencyManager
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
