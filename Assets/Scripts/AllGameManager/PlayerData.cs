a5de6cd549f77d07f710da5e8c4dd83009a781e7
myosotis0423 <shellingford52@gmail.com>
2025-06-18 15:04:29 +0900

    add json, audiosource

--- Assets/Scripts/AllGameManager/PlayerData.cs
+++ Assets/Scripts/AllGameManager/PlayerData.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 [System.Serializable] //Json으로 저장 및 불러오기 할 수 있도록 하기 위해
@@ -8,8 +10,41 @@ public class PlayerData
     public int gold;
     public int stage; //현재 플레이어의 게임진행 단계
     public int attack;
+    public float criticalChance;
     public int level;
 
+    public List<string> ownedWeapons;
+    public string equippedWeapon;
+    public Dictionary<string, int> weaponEnhancementLevels;
+
+    public int autoAttackLevel;
+    public int criticalLevel;
+
+    public Dictionary<string, int> consumableItems;
+    public Dictionary<string, int> statUpgradeLevels;
+
+    private string SavePath => Path.Combine(Application.persistentDataPath, "player_save.json");
+
+    public void Save()//저장
+    {
+        string json = JsonUtility.ToJson(this, true);
+        File.WriteAllText(SavePath, json, Encoding.UTF8);
+        Debug.Log("저장 완료: " + SavePath);
+    }
+
+    public void Load()//불러오기
+    {
+        if (File.Exists(SavePath))
+        {
+            string json = File.ReadAllText(SavePath, Encoding.UTF8);
+            JsonUtility.FromJsonOverwrite(json, this);
+            Debug.Log("불러오기 완료");
+        }
+        else
+        {
+            Debug.Log("저장 파일 없음. 기본값 사용.");
+        }
+    }
     public PlayerData()
     {
         gold = 10000;
